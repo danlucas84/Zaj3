57485c55c7c3b909584729ef2d02a835bbf24aa0
Dell <dandan_21@o2.pl>
2019-05-10 15:12:28 +0200

    dzialanie klas Printer  oraz printerTest przed wprowadzeniem konstruktora z  parametrami

---
+++ src/printerTest.java
@@ -0,0 +1,14 @@
+public class printerTest {
+    public static void main(String[] args) {
+
+        Printer printerInfo = new Printer();
+
+        printerInfo.color = "white";
+        printerInfo.name = "brother";
+        System.out.println("Color drukarki to: " + printerInfo.color);
+        System.out.println("Marka drukarki to: " + printerInfo.name);
+
+
+
+    }
+}
